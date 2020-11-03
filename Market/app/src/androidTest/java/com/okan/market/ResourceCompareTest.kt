d2442799e4c3cfc2e0c885c4503a63870f3e2c13
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-11-03 23:21:47 +0300

    @Before And @After

--- Market/app/src/androidTest/java/com/okan/market/ResourceCompareTest.kt
+++ Market/app/src/androidTest/java/com/okan/market/ResourceCompareTest.kt
@@ -3,6 +3,8 @@ package com.okan.market
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class ResourceCompareTest {
@@ -15,9 +17,27 @@ class ResourceCompareTest {
 
 	private lateinit var resourceCompare: ResourceCompare
 
+
+	/**
+	 * sets up the variables before all test cases
+	 */
+	@Before
+	fun setup() {
+		resourceCompare = ResourceCompare()
+	}
+
+	/**
+	 * removes unnecessary variables after test finishes
+	 * for example you can close your room database
+	 * connection here
+	 */
+	@After
+	fun tearDown() {
+
+	}
+
 	@Test
 	fun stringResourceSameAsGivenString_returnsTrue() {
-		resourceCompare = ResourceCompare()
 		val context = ApplicationProvider.getApplicationContext<Context>()
 		val result = resourceCompare.isEqual(context, R.string.app_name, "Market")
 		assertThat(result).isTrue()
@@ -25,7 +45,6 @@ class ResourceCompareTest {
 
 	@Test
 	fun stringResourceDifferentAsGivenString_returnsFalse() {
-		resourceCompare = ResourceCompare()
 		val context = ApplicationProvider.getApplicationContext<Context>()
 		val result = resourceCompare.isEqual(context, R.string.app_name, "hello")
 		assertThat(result).isFalse()
