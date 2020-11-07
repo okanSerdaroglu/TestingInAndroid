a4a84e09a936be39c773e3f3c2beedc52f3f9bb3
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-11-07 11:43:11 +0300

    Repository Setup

---
+++ Market/app/src/main/java/com/okan/market/repositories/DefaultShoppingRepository.kt
@@ -0,0 +1,50 @@
+package com.okan.market.repositories
+
+import androidx.lifecycle.LiveData
+import com.okan.market.data.local.ShoppingDao
+import com.okan.market.data.local.ShoppingItem
+import com.okan.market.other.Resource
+import com.okan.market.remote.ApiService
+import com.okan.market.remote.responses.ImageResponse
+import javax.inject.Inject
+
+
+class DefaultShoppingRepository
+@Inject
+constructor(
+	private val shoppingDao: ShoppingDao,
+	private val apiService: ApiService
+) : ShoppingRepository {
+	override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+		shoppingDao.insertShoppingItem(shoppingItem)
+	}
+
+	override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+		shoppingDao.deleteShoppingItem(shoppingItem)
+	}
+
+	override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+		return try {
+			val response = apiService.searchForImage(imageQuery)
+			if (response.isSuccessful) {
+				response.body()?.let { imageResponse ->
+					return@let Resource.success(imageResponse)
+				} ?: Resource.error("Unknown error occurred", null)
+			} else {
+				Resource.error("Unknown error occurred", null)
+			}
+		} catch (e: Exception) {
+			Resource.error("Could not reach the server. Check your connection",null)
+		}
+	}
+
+
+	override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+		return shoppingDao.observeAllShoppingItems()
+	}
+
+	override fun observeTotalPrice(): LiveData<Float> {
+		return shoppingDao.observeTotalPrice()
+	}
+
+}
---
+++ Market/app/src/main/java/com/okan/market/repositories/ShoppingRepository.kt
@@ -0,0 +1,28 @@
+package com.okan.market.repositories
+
+import androidx.lifecycle.LiveData
+import com.okan.market.data.local.ShoppingItem
+import com.okan.market.other.Resource
+import com.okan.market.remote.responses.ImageResponse
+import retrofit2.Response
+
+/**
+ * we created this interface, because we implement this
+ * interface both ShoppingRepository and our ShoppingRepositoryTest.
+ * With this method we can implement same methods in both classes.
+ * For this reason it is very good technique for testing
+ */
+
+interface ShoppingRepository {
+
+	suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+	suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
+
+	suspend fun searchForImage(imageQuery: String): Resource<ImageResponse>
+
+	fun observeAllShoppingItems(): LiveData<List<ShoppingItem>>
+
+	fun observeTotalPrice(): LiveData<Float>
+
+}
